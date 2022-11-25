0ccdfcea23cbcec53cdb06b710a7539cf3f1f750
Ivan <pl_st@list.ru>
2022-11-25 14:06:05 +0300

    Накодил

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <p>
+      Количество монет:
+      <span>{{ money }}</span>
+    </p>
+    <hr>
+    <p>Количество кликов:
+      <span>{{ clicks }}</span>
+    </p>
+    <button @click="click">
+      Кликнуть
+    </button>
+
+    <hr>
+    <p>Цена апгрейда:
+      <span>{{ upgradeCost }}</span>
+    </p>
+    <button @click="buyPowerUpgrade">
+      Купить апгрейд
+    </button>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data: () => {
+    return {
+      money: 0,
+      clicks: 0,
+      clickPower: 1,
+      upgradeCost: 6,
+      CLICK_UPGRADE: 1,
+      MONEY_INCOME: 2
+
+    };
+  },
+  methods: {
+    click() {
+      if (this.clicks >= 5) {
+        this.money += this.MONEY_INCOME;
+        this.clicks = 0;
+      }
+      this.clicks += this.clickPower;
+    },
+    buyPowerUpgrade() {
+      if (this.money < this.upgradeCost) {
+        return alert('Нехватает средств')
+      }
+      this.clickPower += this.CLICK_UPGRADE;
+      this.money -= this.upgradeCost
+      this.upgradeCost *= 2;
+    }
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+:root {
+  background: aliceblue;
 }
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+hr {
+  width: 200px;
+}
+
+p {
+  background: lightgray;
+  padding: 20px;
+}
+
 </style>
